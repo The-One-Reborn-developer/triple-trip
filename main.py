ede906be2d867862f9b2fdb11743238e73634a55
The One Reborn <mortonmarrion@gmail.com>
2024-12-08 15:46:36 +0300

    fix: add more wait to bot container so that it loads last

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ async def main():
 
     try:
         logging.info('Waiting for RabbitMQ to be ready')
-        time.sleep(25)
+        time.sleep(30)
         logging.info('Creating database tables')
         create_tables_producer()
     except Exception as e:
