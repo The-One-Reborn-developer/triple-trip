b10c0e42a803a6d6e44310b8f6408bcdc0570489
The One Reborn <mortonmarrion@gmail.com>
2024-12-14 08:03:23 +0300

    refactor: import error message from views

--- app/routes/start.py
+++ app/routes/start.py
@@ -1,12 +1,14 @@
 import logging
 
 from aiogram import Router
+from aiogram.types import Message
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 
 from app.keyboards.start import start_keyboard
 
 from app.views.start import choose_language
+from app.views.errors_en import start_error
 
 from app.tasks.post_user_producer import post_user_producer
 
@@ -15,13 +17,13 @@ start_router = Router()
 
 
 @start_router.message(CommandStart())
-async def start_handler(message, state: FSMContext):
+async def start_handler(message: Message, state: FSMContext):
     await state.clear()
 
     try:
         post_user_producer(message.from_user.id)
     except Exception as e:
-        await message.answer('Error adding you as a user. Please try again or contact support 🙏')
+        await message.answer(start_error())
         logging.error(f'Error in start_router adding user to database: {e}')
 
     await message.answer(
--- app/views/errors_en.py
+++ app/views/errors_en.py
@@ -1,2 +1,6 @@
 def place_photo_error() -> str:
     return 'Send at least one photo 📸'
+
+
+def start_error() -> str:
+    return 'Error adding you as a user. Please try again or contact support 🙏'
