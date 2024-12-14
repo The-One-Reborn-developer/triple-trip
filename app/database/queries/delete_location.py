b99d90a0396ff5342b9650df3252900e9db45a68
The One Reborn <mortonmarrion@gmail.com>
2024-12-14 17:26:46 +0300

    fix: remove await/async from delete_location

--- app/database/queries/delete_location.py
+++ app/database/queries/delete_location.py
@@ -6,11 +6,13 @@ from app.database.models.locations import Location
 from app.database.models.sync_session import sync_session
 
 
-async def delete_location(location_id: int) -> bool:
+def delete_location(location_id: int) -> bool:
     try:
-        await sync_session.execute(
-            delete(Location).where(Location.location_id == location_id)
-        )
+        with sync_session() as session:
+            with session.begin():
+                session.execute(delete(Location).where(Location.id == location_id))
+
+                logging.info(f'Location {location_id} deleted from the database')
         return True
     except Exception as e:
         logging.error(f'Error in delete_location deleting location in the database: {e}')
