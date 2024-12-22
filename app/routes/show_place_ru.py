0ae227a096f3aa410b0d844bf5e7aa1edfb22340
The One Reborn <mortonmarrion@gmail.com>
2024-12-22 21:07:41 +0300

    feat: show locations one by one in show_place_ru route

--- app/routes/show_place_ru.py
+++ app/routes/show_place_ru.py
@@ -16,8 +16,27 @@ async def show_place_handler(callback: CallbackQuery):
     try:
         locations = get_locations_by_country_producer(country_code)
 
-        if locations:
-            await callback.answer(str(locations))
+        if not locations:
+            await callback.answer('В данной стране пока нет локаций 😔', show_alert=True)
+        else:
+            for location in locations:
+                location_details = f'⏫\nНазвание: {location["name"]}\n' \
+                                f'Адрес: {location["address"]}\n'
+                
+                location_media_group = []
+                for photo in location['photos']:
+                    location_media_group.append(
+                        {
+                            'type': 'photo',
+                            'media': photo
+                        }
+                    )
+                
+                await callback.bot.send_media_group(
+                    chat_id=callback.from_user.id,
+                    text=location_details,
+                    media=location_media_group
+                )
     except Exception as e:
         await callback.answer('Произошла ошибка при получении локаций', show_alert=True)
         logging.error(f'Error in show_place_handler getting locations by country {country_code}: {e}')
